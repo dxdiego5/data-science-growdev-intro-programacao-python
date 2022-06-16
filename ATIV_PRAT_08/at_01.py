60d78d10ddb4ed5e0fffbb6764e5aa33c4890dbe
diego felipe da silva bez <dxdiegofelipe@hotmail.com>
2022-06-15 23:14:00 -0400

    alteracao ativ.pratica 08 exercicio 01, adicao do uso da nova funcao conv_monetary, para converter valores floats em moeda brl

--- ATIV_PRAT_08/at_01.py
+++ ATIV_PRAT_08/at_01.py
@@ -1,6 +1,6 @@
 # Enunciado:
 # 1) Procure quem foi a pessoa que mais gastou?
-from functions import convert_data_files
+from functions import convert_data_files, conv_monetary
 
 # chamada da funcao de leitura de arquivo
 data_frame = convert_data_files.init_app()
@@ -15,7 +15,7 @@ for indice, person_row in enumerate(data_frame):
 # ------- END FOR -------
 
 print(f"\n{'=':=^70}")
-print(f' \n O {data_frame[position]["nome"]} {data_frame[position]["sobrenome"]} de {data_frame[position]["idade"]} anos \n teve a maior compra no valor total de R$ {data_frame[position]["compra"]} reais, em {data_frame[position]["ano"]}. \n')
+print(f' \n O {data_frame[position]["nome"]} {data_frame[position]["sobrenome"]} de {data_frame[position]["idade"]} anos \n teve a maior compra no valor total de R$ {conv_monetary.init_app(data_frame[position]["compra"])} reais, em {data_frame[position]["ano"]}. \n')
 print(f"{'=':=^70}\n")
 
 # resposta do print abaixo:
