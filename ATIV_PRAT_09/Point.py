7e3f7b2877e66700d182b24870a5508a40aa710b
diego felipe da silva bez <dxdiegofelipe@hotmail.com>
2022-06-18 22:20:37 -0400

    ativ.pratica 09, exe-Classe Point and Rectangle

--- ATIV_PRAT_09/Point.py
+++ ATIV_PRAT_09/Point.py
@@ -12,3 +12,32 @@
 # valor para um objeto do tipo ponto que indique os valores de x
 # e y para o centro do objeto.
 # h) O valor do centro do objeto deve ser mostrado na tela
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def get_point(self):
+        print(f'x={self.x}, y={self.y}')
+
+
+class Rectangle:
+    def __init__(self):
+        self.point_1 = None
+        self.point_2 = None
+
+    def center(self):
+        point_center = Point()
+        point_center.x = (self.point_1.x + self.point_2.x) / 2
+        point_center.y = (self.point_1.y + self.point_2.y) / 2
+        return point_center
+
+ret = Rectangle()
+
+ret.point_1 = Point(4,3)
+ret.point_2 = Point(10,7)
+
+center_point = ret.center()
+
+center_point.get_point()
